331bb04d2883fa2f96b00f27cb38378bb344602d
jgubbens <jgubbens04@gmail.com>
2025-02-05 18:17:37 -0500

    2.2

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -1,7 +1,6 @@
 export default function Lab1() {
     return (
         <div id="wd-lab1">
-            <h1>Justin Gubbens, CS5610</h1>
             <h2>Lab 1</h2>
             <h3>HTML Examples</h3>
             <div id="wd-h-tag">
